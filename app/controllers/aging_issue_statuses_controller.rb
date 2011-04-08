d8fe0a7024003dee63bda90ff96b2c098a4a0872
Eric Davis <edavis@littlestreamsoftware.com>
2011-04-08 11:49:01 -0700

    [#5072] Add controller to handle listing aging issues

---
+++ app/controllers/aging_issue_statuses_controller.rb
@@ -0,0 +1,8 @@
+class AgingIssueStatusesController < ApplicationController
+  unloadable
+
+  def index
+    render :text => 'hi'
+  end
+  
+end
---
+++ test/integration/list_aging_issues_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class ListAgingIssuesTest < ActionController::IntegrationTest
+  def setup
+    configure_plugin
+    @user = User.generate!(:password => 'test', :password_confirmation => 'test', :admin => false).reload
+    @project = Project.generate!
+    @role = Role.generate!(:permissions => [:view_issues])
+    User.add_to_project(@user, @project, @role)
+    @status = IssueStatus.generate!
+    @old_status = IssueStatus.generate!
+
+    @warning_issue1 = Issue.generate_for_project!(@project, :status => @status, :created_on => 30.days.ago)
+    @warning_issue2 = Issue.generate_for_project!(@project, :status => @status, :created_on => 30.days.ago)
+    @error_issue1 = Issue.generate_for_project!(@project, :status => @status, :created_on => 30.days.ago)
+    @error_issue2 = Issue.generate_for_project!(@project, :status => @status, :created_on => 30.days.ago)
+    @not_aging_issue = Issue.generate_for_project!(@project, :status => @status, :created_on => 30.days.ago)
+
+    @not_visible_project = Project.generate!
+    @not_visible_issue = Issue.generate_for_project!(@not_visible_project, :status => @status, :created_on => 30.days.ago)
+
+    generate_journal_for_status_change(@warning_issue1, 8.days.ago)
+    generate_journal_for_status_change(@warning_issue2, 9.days.ago)
+    generate_journal_for_status_change(@error_issue1, 16.days.ago)
+    generate_journal_for_status_change(@error_issue2, 17.days.ago)
+    generate_journal_for_status_change(@not_aging_issue, 1.day.ago)
+    generate_journal_for_status_change(@not_visible_issue, 29.days.ago)
+
+    login_as(@user.login, 'test')
+    visit_home
+  end
+
+  context "browsing to the list of aging issues" do
+    should "be successful" do
+      visit '/aging_issue_statuses'
+      assert_response :success
+    end
+    
+    should "show a list of issues"
+    should "show orange next to warning issues"
+    should "show red next to warning issues"
+    should "not show issues that aren't aging"
+    should "not show issues on hidden projects"
+  end
+end
