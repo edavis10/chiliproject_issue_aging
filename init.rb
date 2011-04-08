31fe6ea1a2933a8b9bd2b465a75c6b9022c0b865
Eric Davis <edavis@littlestreamsoftware.com>
2011-04-08 10:20:28 -0700

    [#5072] Add authorship

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :chiliproject_issue_aging do
-  name 'Chiliproject Issue Aging plugin'
-  author 'Author name'
-  description 'This is a plugin for ChiliProject'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Issue Aging'
+  author 'Eric Davis'
+  url "https://projects.littlestreamsoftware.com/projects/chiliproject_issue_aging"
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Plugin to help manage aging issues.'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '1.0.0'
 end
