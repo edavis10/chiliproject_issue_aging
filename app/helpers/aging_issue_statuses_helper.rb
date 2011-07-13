1c5509da184d2870ff8c8dc844c340b1515b614b
Eric Davis <edavis@littlestreamsoftware.com>
2011-07-13 11:42:08 -0700

    [#5858] Change how the last status change is listed

--- app/helpers/aging_issue_statuses_helper.rb
+++ app/helpers/aging_issue_statuses_helper.rb
@@ -6,4 +6,17 @@ module AgingIssueStatusesHelper
       "#{link_to_project(project.root)} &#187; #{link_to_project(project)}"
     end
   end
+
+  def number_of_days_ago(date)
+    (Date.today - date.to_date).to_i
+  end
+
+  def aging_issue_title(issue)
+    journal = issue.last_status_change_journal
+
+    if journal
+      days_ago = (Date.today - journal.created_on.to_date).to_i
+      "#{l(:issue_aging_text_days_ago, :count => days_ago)} (#{ format_time(journal.created_at) })"
+    end
+  end
 end
